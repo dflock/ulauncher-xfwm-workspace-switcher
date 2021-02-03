67acc09767dfdbdf8c9b27f22f4a6e5d20116565
Duncan Lock <duncan.lock@gmail.com>
2021-02-02 23:28:57 -0800

    Add digit & dash shortcuts, speedup
    Quite a large refactor to add digit & dash shortcuts, and to reduce the amount of shelling out.

--- README.md
+++ README.md
@@ -6,6 +6,12 @@ This is a plugin for [uLauncher](https://ulauncher.io/) that lets you search & s
 
 The default keyword in `w`, so just run Ulauncher, then type `w <query>` to filter the workspace list, then select a workspace from the list to switch to that workspace.
 
+## Shortcuts
+
+If you type `<keyword> -`, it'll offer to take you back to your previous workspace - i.e. the one you were on before this one. It will save the current workspace into `$HOME/.lws` just before switching, so this will only work if you use this switcher - it won't know about workspace switches you make by other means. This is intended to work similarly to `cd -`, or `pushd/popd`.
+
+If you type a number, e.g. `<keyword> 12` it'll offer to take you directly to workspace 12.
+
 I used workspace icons from the [Obsidian Icon pack](https://github.com/madmaxms/iconpack-obsidian)
 
 ## Requirements
--- main.py
+++ main.py
@@ -7,33 +7,85 @@ from ulauncher.api.shared.action.HideWindowAction import HideWindowAction
 from ulauncher.api.shared.action.RunScriptAction import RunScriptAction
 
 import subprocess
+from os import path
 
-class XfwmWorkspaceExtension(Extension):
+class WorkspaceExtension(Extension):
 
     def __init__(self):
-        super(XfwmWorkspaceExtension, self).__init__()
+        super(WorkspaceExtension, self).__init__()
         self.subscribe(KeywordQueryEvent, KeywordQueryEventListener())
 
 
 class KeywordQueryEventListener(EventListener):
+
+    def __init__(self):
+        self.curr_ws = [None, None]
+        self.lws = '$HOME/.lws'
+        self.ws_list = None
+
+        self.get_current_ws()
+        self.init_lws()
+        self.get_ws_list()
+    
+    def get_current_ws(self):
+        # Get current workspace ID & name: [id, name]
+        self.curr_ws = subprocess.run(['wmctrl -d | grep -F " * DG:" | awk \'{$2=$3=$4=$5=$6=$7=$8=$9=""; print $0}\''], capture_output=True, shell=True, text=True).stdout.strip().split(maxsplit=1)
+    
+    def lws_save(self):
+        # Return shell command to save the current workspace
+        return f'echo -n "{self.curr_ws[0]}    {self.curr_ws[1]}" > "{self.lws}"'
+
+    def init_lws(self):
+        # If no "$HOME/.lws", then create & populate 
+        subprocess.run([f'if [ ! -f "{self.lws}" ]; then {self.lws_save()}; fi'], shell=True)
+
+    def get_last_ws(self):
+        # Read the last workspace into self.last_ws & return it
+        ws = None
+        with open(path.expandvars(self.lws), 'r') as file:
+            ws = file.read()
+        return ws.split(maxsplit=1)
+
+    def get_ws_list(self):
+        # Get list of all workspaces
+        result = subprocess.run(['wmctrl -d | awk \'{$1=$2=$3=$4=$5=$6=$7=$8=$9=""; print $0}\''], capture_output=True, shell=True, text=True).stdout
+        self.ws_list = [y for y in (x.strip() for x in result.splitlines()) if y]
 
     def on_event(self, event, extension):
         keyword = event.get_keyword()
         search = str(event.get_argument() or '').lower().strip()
 
         items = []
-        result = subprocess.run(['wmctrl -d | awk \'{$1=$2=$3=$4=$5=$6=$7=$8=$9=""; print $0}\''], capture_output=True, shell=True, text=True).stdout
-        ws_list = [y for y in (x.strip() for x in result.splitlines()) if y]
 
-        for ws_idx, ws_name in enumerate(ws_list):
-            if search == '' or search in ws_name.lower():
-                items.append(ExtensionResultItem(icon='images/workspace-switcher-top-left.svg',
-                                                # Workaround for https://github.com/Ulauncher/Ulauncher/issues/587
-                                                name=ws_name.replace('&', '&amp;') if search else ws_name,
-                                                description=f'Workspace Name: {ws_name}, Workspace Id: {ws_idx}',
-                                                on_enter=RunScriptAction(f'wmctrl -s {ws_idx}')))
+        self.get_current_ws()
+
+        if search.isdigit():
+            # If search is a number, then shortcut to that workspace.
+            action = f'wmctrl -s {abs(int(search) - 1)} && {self.lws_save()}'
+            items.append(ExtensionResultItem(icon='images/workspace-switcher-top-left.svg',
+                                            name=f'Workspace {search}',
+                                            description=f'Go to workspce {search}',
+                                            on_enter=RunScriptAction(action)))
+        elif search == '-':
+            # Shortcut to return to your previous workspace, like `cd -` does.
+            lws = self.get_last_ws()
+            action = f'wmctrl -s "{lws[0]}" && {self.lws_save()}'
+            items.append(ExtensionResultItem(icon='images/workspace-switcher-top-left.svg',
+                                            name=f'Go back to last used workspace',
+                                            description=f'Workspace Name: {lws[1]}, Workspace Id: {int(lws[0]) + 1}',
+                                            on_enter=RunScriptAction(action)))
+        else:
+            # Otherwise, match on workspace names
+            for ws_idx, ws_name in enumerate(self.ws_list):
+                if search == '' or search in ws_name.lower():
+                    action = f'wmctrl -s {ws_idx} && {self.lws_save()}'
+                    items.append(ExtensionResultItem(icon='images/workspace-switcher-top-left.svg',
+                                                    # Workaround for https://github.com/Ulauncher/Ulauncher/issues/587
+                                                    name=ws_name.replace('&', '&amp;') if search else ws_name,
+                                                    description=f'Workspace Name: {ws_name}, Workspace Id: {int(ws_idx) + 1}',
+                                                    on_enter=RunScriptAction(action)))
 
         return RenderResultListAction(items)
 
 if __name__ == '__main__':
-    XfwmWorkspaceExtension().run()
+    WorkspaceExtension().run()
